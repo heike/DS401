cb82d06dad59741a5d370f99e3fc32c9b4655fec
Heike Hofmann <hofmann@iastate.edu>
2022-02-23 11:39:44 -0600

    update documentation

--- R/extract_median_na_proportion.R
+++ R/extract_median_na_proportion.R
@@ -1,18 +1,19 @@
-#` Feature Extraction: Median of the proportion of NA values present in the middle of a 3d scan, 
-#` from 20 different y values.
-#` 
-#` One of the main problems with 3d scans are the feathering that occurs within the middle of the scan.
-#` Here, we quantify this feathering by taking samples at 20 specific y values in the surface matrix
-#` that are in the middle 2/3 of the image. Then with these rows of the surface matrix we measure
-#` the proportion of missing values in each of those 20 rows. We then take the median proportion of missing values
-#` of those rows, which will give us 
-#` a good measure to quantify the amount of feathering in a 3d scan. 
-#` @param x3p scan in x3p format
-#` @return median percentage of missing values
-#` @export
-#` @examples
-#` data(fau277_bb_12)
-#` extract_median_na_proportion(fau277_bb_12) 
+#' Feature Extraction: Median of the proportion of NA values present in the middle of a 3d scan,
+#' from 20 different y values.
+#'
+#' One of the main problems with 3d scans are the feathering that occurs within the middle of the scan.
+#' Here, we quantify this feathering by taking samples at 20 specific y values in the surface matrix
+#' that are in the middle 2/3 of the image. Then with these rows of the surface matrix we measure
+#' the proportion of missing values in each of those 20 rows. We then take the median proportion of missing values
+#' of those rows, which will give us
+#' a good measure to quantify the amount of feathering in a 3d scan.
+#' @param x3p scan in x3p format
+#' @return median percentage of missing values
+#' @importFrom stats median
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' extract_median_na_proportion(fau277_bb_l2)
 extract_median_na_proportion <- function(x3p) {
 
 SurfaceMatrix <- x3p$surface.matrix
