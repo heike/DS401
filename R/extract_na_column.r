ca9d081b96a1a7abe1e5d17948f5fe4168010770
Heike Hofmann <hofmann@iastate.edu>
2022-02-23 12:19:05 -0600

    upload second scan

---
+++ R/extract_na_column.r
@@ -0,0 +1,46 @@
+#' Feature extraction: percentage of missing values by column
+#'
+#' A good quality of 3d scans is crucial for an assessment of similarity of
+#' striation marks between different bullets.
+#' Here, we measure the percentage of missing values in the scan. Generally,
+#' a higher percentage of missing values is associated with a lower scan quality.
+#' @param x3p scan in x3p format
+#' @return percentage of missing values in each column of the scan's surface matrix
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' nas <- extract_na_column(fau277_bb_l2) # this scan has a particularly high percentage of missing values
+#' plot(nas) # the 'feathering' becomes obvious in the spikes of the missing values
+extract_na_column <- function(x3p) {
+  stopifnot(class(x3p) == "x3p")
+
+  dims <- dim(x3p$surface.matrix)
+  # row sum will provide a summary of each column in the scan matrix (transposed values)
+  nas <- rowSums(is.na(x3p$surface.matrix))
+
+  return(nas/dims[2]*100)
+}
+
+
+#' Feature extraction: percentage of missing values by column
+#'
+#' A good quality of 3d scans is crucial for an assessment of similarity of
+#' striation marks between different bullets.
+#' Here, we measure the percentage of missing values in the scan. Generally,
+#' a higher percentage of missing values is associated with a lower scan quality.
+#' @param x3p scan in x3p format
+#' @return percentage of missing values in each row of the scan's surface matrix
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' nas <- extract_na_row(fau277_bb_l2) # this scan has a particularly high percentage of missing values
+#' plot(nas) # interesting pattern: we would expect a similar pattern for all scans (going from very low percentage of missing values at the top of a scan to a fairly high percentage along the bottom)
+extract_na_row <- function(x3p) {
+  stopifnot(class(x3p) == "x3p")
+
+    dims <- dim(x3p$surface.matrix)
+  # row sum will provide a summary of each column in the scan matrix (transposed values)
+  nas <- colSums(is.na(x3p$surface.matrix))
+
+  return(nas/dims[1]*100)
+}
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,7 +3,12 @@
 # need to think about how best to add some data to the package
 
 library(x3ptools)
+library(magrittr)
 fau277_bb_l2 <- read_x3p("data-raw/fau277-bb-l2.x3p")
 usethis::use_data(fau277_bb_l2, overwrite = TRUE)
 
 #usethis::use_data(DATASET, overwrite = TRUE)
+
+fau001_ba_l1 <- read_x3p("data-raw/fau001-ba-l1.x3p")
+fau001_ba_l1 <- fau001_ba_l1 %>% x3p_sample(m=3)
+usethis::use_data(fau001_ba_l1, overwrite = TRUE)
