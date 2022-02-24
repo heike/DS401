20140c429002aa06a62bdeeba0f6b8874e1e5f52
jbaalson <jbaalson@iastate.edu>
2022-02-23 23:00:56 -0600

    Update assess_median_na_proportion.R

--- R/assess_median_na_proportion.R
+++ R/assess_median_na_proportion.R
@@ -1,17 +1,15 @@
 #' Feature Extraction: Median of the proportion of NA values present in the middle of a 3d scan,
-#' from 20 different y values.
+#' from a specific number of different y values.
 #'
 #' One of the main problems with 3d scans are the feathering that occurs within the middle of the scan.
-#' Here, we quantify this feathering by taking samples at 20 specific y values in the surface matrix
-#' that are in the middle 2/3 of the image. Then with these rows of the surface matrix we measure
-#' the proportion of missing values in each of those 20 rows. We then take the median proportion of missing values
-#' of those rows, which will give us
-#' a good measure to quantify the amount of feathering in a 3d scan.
+#' Here, we quantify this feathering by taking samples at specific number of y values in the surface matrix
+#' that are in a certain percentage of the middle of the 3d scan. Then with these rows of the surface matrix we measure
+#' the proportion of missing values in each of those rows. We then take the median proportion of missing values
+#' of those rows, which will give us a good measure to quantify the amount of feathering in a 3d scan.
 #' @param x3p scan in x3p format
-#' @param chopoff numeric value between 0 and .5 - how many values do we need to chop off from either side of the scan?
+#' @param chopoff numeric value between 0 and .5 - The percentage of values to chop off of both sides of the scan
 #' @param numlines Number of horizontal lines across the scan to find the median proportion of na values from
-#' @return median percentage of missing values # HH: percentage or proportion?
-#' HH: I believe, that what you are calculating is actually a lot more complicated than an overall median of missing values.
+#' @return median proportion of missing values
 #' @importFrom stats median
 #' @export
 #' @examples
@@ -23,7 +21,7 @@ assess_median_na_proportion <- function(x3p, chopoff = 1/6, numlines = 20) {
   
   SurfaceMatrix <- x3p$surface.matrix
 
-  NumberOfLines <- numlines - 2 # HH: where does the 18 come from? ## from the bottom up, + 2 lines
+  NumberOfLines <- numlines - 2
 
   NumberofYIncrements <- x3p$header.info$sizeY
 
