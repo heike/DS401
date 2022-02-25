7e15c19ef69ff50fdf0784b8312a54a27fff25e9
yangfanc <60075569+yangfanc@users.noreply.github.com>
2022-02-25 17:29:09 -0600

    Update assess_bottom_prob.R

--- R/assess_bottom_prob.R
+++ R/assess_bottom_prob.R
@@ -7,7 +7,6 @@ devtools::install_github("heike/DS401")
 
 #library
 library(x3ptools)
-
 library(Rcmdr)
 library(imager)
 library(DS401)
@@ -18,22 +17,28 @@ library(imagemetrics)
 
 
 
-#place:
-#setwd("~/Desktop/Learning/isu/spring2022/DS401/DS401/man")
-#png name:
-#LAPD-FAU227-BD-L2.png
-
-#try to read
+#read the x3p and load the the image.
 x3ps <- dir(pattern = "x3p", recursive = TRUE, full.names = TRUE)
 x3p_image(fau277_bb_l2, file = "man/figures/fau277_bb_l2.png")
 x3p_image(fau001_ba_l1, file = "man/figures/fau001_ba_l1.png")
 
 extract_na(fau277_bb_l2)
 
+#' ideal:
+#' we guess the .png have some missing data but we are not sure about 
+#' how many missing and what probability in the NA prability
+#' Also, we are guess that we the right of the .png and bottom of .png  have the missing value are overlapping.
+#' we mat need to confused about the overlapping the missing data.
+#' we can be caseful the botton of the missing data and adjust them and calculate their probabilities.
 
+#' In this function, my idea is that get the bootom missing data and get the probality of the bottom
+#' step:
 #' the missing value in the bottom size
-#' we can be find and calcultor and get the missing value to calcultor
-#' then, we can be calcultor the porobability
+#' find the NA value and sum of that
+#' sum of the bottom data 
+#' calculator the probability
+
+
 
 #' @param x3p scan in x3p format
 #' @return percentage of missing values in each column of the scan's surface matrix
@@ -47,66 +52,68 @@ extract_na(fau277_bb_l2)
 #' plot(nas_good) # similar pattern to above, but low values for most of the scan
 #'
 
-#make a function in the column NA data
-#extract_na_column <- function(x3p) {
- # stopifnot(class(x3p) == "x3p")
 
-# dims <- dim(x3p$surface.matrix)
-# rows <- extract_na_row(x3p)*dims[1]/100
-
- # return(nas/prod(dims)*100)
-
-
-  #the file mean the png of .x3p file
- # file <-  x3p_to_df(x3p)
-
-
-  #.png  - missing value
-#  right <- file %>% mutate(place= (file[rowSums(is.na(file)) > 40, ])
-                             
-
-  #sum of the missing data 
-#  missing <- sum(right)
-  
-  #number of value in the right of the png
- # value <- length(which(right$place=prod(dims)*.2))
-  
-  #present
- # present < (missing/value) * 100
-  
-#  return(present)
-
-#}
-
+#' this part is find the probabiliy of bottom
 prob <-function(x3p){
     stopifnot(class(x3p) == "x3p")
 
  dims <- dim(x3p$surface.matrix)
+ # ordered from top of scan to bottom
  rows <- extract_na_row(x3p)*dims[1]/100
 
- # return(nas/prod(dims)*100)
 
 
   #the file mean the png of .x3p file
   file <-  x3p_to_df(x3p)
-  
-  #In order to get the positions of each column
-  is.na(file)
-  apply(is.na(file), 2, which)
-  
- #get the bottom pixel
-  get <- getImagePixels(file, side = 0.3)
-  str(get)
-  sum_get<-sum(get)
+ 
 
-  #calculator the max
-  suggestMaximumBins(file)
-  file.prob = calculateHisto(reference_vector = file$reference_vector,
-                             neighbour_vector = file$neighbour_vector,
-                             nbins = 0.3)
-  getprob <- (sum_get/file.prob)*100
-  return(getprob)
+# calculating the product of dimensions of dataframe 
+totalcells = prod(dim(file))
+ 
+  #sum of calculator missing data in each column
+ sum_col <- apply(file, 2, sum, na. rm = TRUE )
+ 
+ 
+ 
+ #get the bottom  NA pixel
+ # get <- getImagePixels(file, side = 72)
+ # str(get)
+  #sum_get<-sum(get)
+
+  #calculator the max missing value
+#  suggestMaximumBins(file)
+#  file.prob = calculateHisto(reference_vector = file$reference_vector,
+ #                            neighbour_vector = file$neighbour_vector,
+  #                           nbins = 72)
+ #
+percentage = (sum_col * 100 )/(totalcells)
+ 
+  return(percentage)
   }
-                           
+       
 
-#x3p_image(fau277_bb_l2, file = "man/figures/fau277_bb_l2.png")
+
+#max probability in each column
+max_prob <-function(x3p){
+    stopifnot(class(x3p) == "x3p")
+
+ dims <- dim(x3p$surface.matrix)
+ # ordered from top of scan to bottom
+ rows <- extract_na_row(x3p)*dims[1]/100
+
+
+
+  #the file mean the png of .x3p file
+  file <-  x3p_to_df(x3p)
+ 
+#Find the max value NA  data in each column
+  is.na(file)
+  max_value <- apply(file,max,na.rm=TRUE)
+ # calculating the product of dimensions of dataframe 
+totalcells = prod(dim(file))
+ 
+ percentage = (max_value * 100)/(totalcells)
+ return(percentage)
+ 
+  }
+
