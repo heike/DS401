6162b819390e66813f19e03d19dc1dc2b5053f15
Heike Hofmann <hofmann@iastate.edu>
2022-02-28 10:10:57 -0600

    should be x3p not test

--- R/find_brightness.R
+++ R/find_brightness.R
@@ -1,15 +1,14 @@
-library(tidyverse)
-library(DS401)
-library(x3ptools)
-
+#' Get the lighting from an x3p object
+#'
 #' @param x3p is a scan in x3p format
+#' @export
 #' @return the brightness of the file from the Comment attribute of the x3p object
 find_brightness <- function(x3p)
 {
   string <- x3p$general.info$Comment
-  
-  brightness <- trimws(str_extract(test$general.info$Comment, "(?<=lighting: )(.*)(?= threshold)"))
-  
+
+  brightness <- trimws(str_extract(x3p$general.info$Comment, "(?<=lighting: )(.*)(?= threshold)"))
+
   return(brightness)
 }
 
