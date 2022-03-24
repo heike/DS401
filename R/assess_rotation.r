70fe40f3824f3a3a86ca12c011544e1b4bbbfe21
Heike Hofmann <hofmann@iastate.edu>
2022-03-24 16:14:58 -0500

    update rotational assessment

---
+++ R/assess_rotation.r
@@ -0,0 +1,37 @@
+#' Assess rotational imbalance of a scan
+#'
+#' A good quality of 3d scans is crucial for an assessment of similarity of
+#' striation marks between different bullets.
+#' Here, we assess rotational imbalance - we measure this as the ratio between
+#' the difference in the number of missing values on the left and the right of a scan at two different heights.
+#' A ratio of 1 would indicate a balance in the proportion of missing values between the left hand side and the right hand side of a scan.
+#' Large deviations of 1 indicate an imbalance to the left or right. A log transform of the ratio akes the result symmetric around 0. Large deviations from 0 indicate a stronger deviation.
+#' @param x3p scan in x3p format
+#' @param width how wide are the bands that we want to compare on the left and the right?
+#' @return absolute value of the log of the ratio of the number of missing values on the left compared to the right side at the bottom and the middle of the scan.
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' assess_rotation(fau277_bb_l2) # this scan has a particularly high percentage of missing values
+#' assess_rotation(fau001_ba_l1) # good scan
+assess_rotation <- function(x3p, width=0.2) {
+  stopifnot(class(x3p) == "x3p")
+  stopifnot(probs >= 0, probs <= 1)
+  stopifnot(width > 0, width < 0.5)
+
+  dims <- dim(x3p$surface.matrix)
+  band <- max(floor(width*dims[1]),1) # round down, but make sure we have at least one column of values
+  half <- round(dims[2]/2)
+
+  middle <- rowMeans(x3p$surface.matrix[,half+(-5:5)], na.rm=TRUE)
+  bottom <- rowMeans(x3p$surface.matrix[,dims[2]-0:10], na.rm=TRUE)
+
+
+  na_left <- sum(is.na(middle[1:band]))
+  na_right <- sum(is.na(middle[dims[2]+1-band:1]))
+#browser()
+  na_left_bottom <- sum(is.na(bottom[1:band]))
+  na_right_bottom <- sum(is.na(bottom[dims[2]+1-band:1]))
+
+  return(abs(log((na_left+1)/(na_left_bottom+1)/((na_right+1)/(na_right_bottom+1)) )))
+}
---
+++ R/extract_median_na_proportion.R
@@ -0,0 +1,58 @@
+#' Feature Extraction: Median of the proportion of NA values present in the middle of a 3d scan,
+#' from 20 different y values.
+#'
+#' One of the main problems with 3d scans are the feathering that occurs within the middle of the scan.
+#' Here, we quantify this feathering by taking samples at 20 specific y values in the surface matrix
+#' that are in the middle 2/3 of the image. Then with these rows of the surface matrix we measure
+#' the proportion of missing values in each of those 20 rows. We then take the median proportion of missing values
+#' of those rows, which will give us
+#' a good measure to quantify the amount of feathering in a 3d scan.
+#' @param x3p scan in x3p format
+#' @param chopoff numeric value between 0 and .5 - how many values do we need to chop off from either side of the scan?
+#' @return median percentage of missing values # HH: percentage or proportion?
+#' HH: I believe, that what you are calculating is actually a lot more complicated than an overall median of missing values.
+#' @importFrom stats median
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' extract_median_na_proportion(fau277_bb_l2)
+extract_median_na_proportion <- function(x3p, chopoff = 1/6) {
+
+  SurfaceMatrix <- x3p$surface.matrix
+
+  NumberOfLines <- 18 # HH: where does the 18 come from? ## from the bottom up, + 2 lines
+
+  NumberofYIncrements <- x3p$header.info$sizeY
+
+  NumberOfIncrementsBetweenLines <- floor(NumberofYIncrements/NumberOfLines)
+
+  MaximumX <- x3p$header.info$sizeX*x3p$header.info$incrementX
+
+  LowerXBound <- MaximumX * chopoff
+
+  UpperXBound <- MaximumX - (MaximumX * chopoff)
+
+  LowerYBound <- 0
+
+  UpperYBound <- x3p$header.info$sizeY*x3p$header.info$incrementY
+
+  IndexLowerXBound <- floor(LowerXBound/x3p$header.info$incrementX)
+
+  IndexUpperXBound <- ceiling(UpperXBound/x3p$header.info$incrementX)
+
+  SurfaceMatrix <- SurfaceMatrix[IndexLowerXBound:IndexUpperXBound,]
+
+  ProportionNA <- vector(mode = "numeric",length = NumberOfLines+1)
+
+  ProportionNA[1] <- mean(is.na(SurfaceMatrix[,1]))
+
+  for(i in 1:(NumberOfLines-1)){
+    ProportionNA[i+1] <- mean(is.na(SurfaceMatrix[,i*NumberOfIncrementsBetweenLines]))
+  }
+
+  ProportionNA[NumberOfLines + 1] <- mean(is.na(SurfaceMatrix[,x3p$header.info$sizeY]))
+
+  Median <- median(ProportionNA)
+  return(Median)
+
+}
