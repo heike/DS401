481f36b3f400cebba00bda215ff289be79d44429
Jacob Baalson <59940231+jbaalson@users.noreply.github.com>
2022-03-28 12:23:32 -0500

    Add files via upload

---
+++ R/assess_92_percentile_na_proportion.R
@@ -0,0 +1,60 @@
+#' Feature Extraction: 92nd percentile of the proportion of NA values present in the middle of a 3d scan,
+#' from a specific number of different y values.
+#'
+#' One of the main problems with 3d scans are the feathering that occurs within the middle of the scan.
+#' Here, we quantify this feathering by taking samples at specific number of y values in the surface matrix
+#' that are in a certain percentage of the middle of the 3d scan. Then with these rows of the surface matrix we measure
+#' the proportion of missing values in each of those rows. We then take the 92nd percentile proportion of missing values
+#' of those rows, which will give us a good measure to quantify the amount of feathering in a 3d scan.
+#' @param x3p scan in x3p format
+#' @param chopoff numeric value between 0 and .5 - The percentage of values to chop off of both sides of the scan
+#' @param numlines Number of horizontal lines across the scan to find the median proportion of na values from
+#' @return median proportion of missing values
+#' @import
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' assess_92_percentile_na_proportion(fau277_bb_l2)
+assess_92_percentile_na_proportion<- function(x3p, chopoff = 1/6, numlines = 200) {
+  
+  stopifnot(class(x3p) == "x3p")
+  
+  SurfaceMatrix <- x3p$surface.matrix
+  
+  NumberOfLines <- numlines - 2
+  
+  NumberofYIncrements <- x3p$header.info$sizeY
+  
+  NumberOfIncrementsBetweenLines <- floor(NumberofYIncrements/NumberOfLines)
+  
+  MaximumX <- x3p$header.info$sizeX*x3p$header.info$incrementX
+  
+  LowerXBound <- MaximumX * chopoff
+  
+  UpperXBound <- MaximumX - (MaximumX * chopoff)
+  
+  LowerYBound <- 0
+  
+  UpperYBound <- x3p$header.info$sizeY*x3p$header.info$incrementY
+  
+  IndexLowerXBound <- floor(LowerXBound/x3p$header.info$incrementX)
+  
+  IndexUpperXBound <- ceiling(UpperXBound/x3p$header.info$incrementX)
+  
+  SurfaceMatrix <- SurfaceMatrix[IndexLowerXBound:IndexUpperXBound,]
+  
+  ProportionNA <- vector(mode = "numeric",length = NumberOfLines+1)
+  
+  ProportionNA[1] <- mean(is.na(SurfaceMatrix[,1]))
+  
+  for(i in 1:(NumberOfLines-1)){
+    ProportionNA[i+1] <- mean(is.na(SurfaceMatrix[,i*NumberOfIncrementsBetweenLines]))
+  }
+  
+  ProportionNA[NumberOfLines + 1] <- mean(is.na(SurfaceMatrix[,x3p$header.info$sizeY]))
+  
+  percentile <- quantile(ProportionNA, probs = 0.92)
+  
+  return(percentile)
+  
+}
