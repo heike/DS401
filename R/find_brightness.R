1ec2d162fab2ed70ba7922953246e238a7c767c5
Heike Hofmann <hofmann@iastate.edu>
2022-04-27 15:14:43 -0500

    find_lighting added

--- R/find_brightness.R
+++ R/find_brightness.R
@@ -14,3 +14,22 @@ find_brightness <- function(x3p)
 }
 
 
+#' Get lighting protocol from the name of an x3p object
+#'
+#' @param name name of an x3p object
+#' @export
+#' @return 1 for 20% autolight, and 2 for 20% x 10 autolight ('flooding')
+find_lighting <- function(name)
+{
+  light_start <- str_locate(name, "auto")
+  light_end <- str_locate(name, "threshold")
+  lighting <- str_sub(name, light_start[,1], light_end[,1]-1)
+  lighting=gsub(" -","",lighting)
+  lighting = trimws(lighting)
+
+  lighting = gsub(" ","", lighting)
+  lighting = ifelse(lighting=="autolightleftimage+20%", 1, 2)
+
+
+  return(lighting)
+}
