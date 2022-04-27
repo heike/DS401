dd340304659da347adf079dafccd9bf5c326f6da
jbaalson <jbaalson@iastate.edu>
2022-04-27 13:11:14 -0500

    Update predict_quality.R

--- R/predict_quality.R
+++ R/predict_quality.R
@@ -1,36 +1,56 @@
-#' Predicts the quality of a vector of x3p scan's by outputing the random forest predicted probability that a scan's quality is good.
-#'
-#' @param x3pvector a vector of x3p objects to be classified as good or bad quality scans
-#' @return a vector of random forest probability scores
-#' @import randomForest
+#' Predicts the probability that a x3p scan will be good and also predicts the reason why it is bad quality if it is predicted to be bad quality.
+#' 
+#' @param x3pobjectvector a vector of x3p objects to be classified as good or bad quality scans
+#' @param x3pnamevector a vector of names of x3p files
+#' @return a dataframe containing a vector of predicted probabilities of a scan being good and its predicted quality_type
+#' @importFrom randomForest predict
 #' @export
 #' @examples
-predict_quality<- function(x3pvector) {
-
-  for(i in 1:(length(x3pvector)))
+predict_quality<- function(x3pobjectvector, x3pnamevector) {
+  
+  for(i in 1:(length(x3pobjectvector)))
   {
-    stopifnot(class(x3pvector[i]) == "x3p")
+    stopifnot(class(x3pobjectvector[i]) == "x3p")
   }
-
-  data_assess_percentile_na_proportion <- c(rep(0, times = length(x3pvector)))
-  data_assess_bottomempty <- c(rep(0, times = length(x3pvector)))
-  data_assess_col_na <- c(rep(0,times = length(x3pvector)))
-  data_extract_na <- c(rep(0,times = length(x3pvector)))
-  data_assess_middle_na_proportion <- c(rep(0, times = length(x3pvector)))
-
-  newdata <- data.frame(data_assess_percentile_na_proportion, data_assess_col_na, data_extract_na, data_assess_middle_na_proportion)
-
-
+  
+  data_assess_percentile_na_proportion <- c(rep(0, times = length(x3pobjectvector)))
+  data_assess_bottomempty <- c(rep(0, times = length(x3pobjectvector)))
+  data_assess_col_na <- c(rep(0,times = length(x3pobjectvector)))
+  data_extract_na <- c(rep(0,times = length(x3pobjectvector)))
+  data_assess_middle_na_proportion <- c(rep(0, times = length(x3pobjectvector)))
+  data_assess_rotation <- c(rep(0, times = length(x3pobjectvector)))
+  data_lighting_protocol <- c(rep(0, times = length(x3pobjectvector)))
+  
+  newdata <- data.frame(data_assess_percentile_na_proportion, data_assess_col_na, data_extract_na, data_assess_middle_na_proportion, data_assess_rotation, data_lighting_protocol)
+  
+  
   for(i in 1:dim(newdata)[1]){
-    newdata$data_assess_percentile_na_proportion[i] <- assess_median_na_proportion(x3pvector[i])
-    newdata$data_assess_bottomempty[i] <- assess_bottomempty(x3pvector[i])
-    newdata$data_assess_col_na[i] <- assess_col_na(x3pvector[i])
-    newdata$data_extract_na[i] <- extract_na(x3pvector[i])
-    newdata$data_assess_middle_na_proportion[i] <- assess_middle_na_proportion(x3pvector[i])
+    newdata$data_assess_percentile_na_proportion[i] <- assess_median_na_proportion(x3pobjectvector[i])
+    newdata$data_assess_bottomempty[i] <- assess_bottomempty(x3pobjectvector[i])
+    newdata$data_assess_col_na[i] <- assess_col_na(x3pobjectvector[i])
+    newdata$data_extract_na[i] <- extract_na(x3pobjectvector[i])
+    newdata$data_assess_middle_na_proportion[i] <- assess_middle_na_proportion(x3pobjectvector[i])
+    newdata$data_assess_rotation[i] <- assess_rotation(x3pobjectvector[i])
+    newdata$data_lighting_protocol[i] <- lighting_protocol(x3pnamevector[i])
   }
-
-  return(predict(randomForest, newdata = newdata, type = "prob")[,2])
-
-
+  
+  Quality <- predict(randomForest, newdata = newdata, type = prob[,2])
+  
+  Quality_Type <- c(rep("", times = length(x3pobjectvector)))
+  
+  for(i in 1:dim(newdata)){
+    if(Quality[i] <= 0.57){
+      Quality_Type[i] <- predict(randomForest2, newdata = newdata[i], type = 'response')
+    } else{
+      Quality_Type[i] <- "good"
+    }
+    
+  }
+  
+  output <- data.frame(Quality, Quality_Type)
+  
+  return(output)
+  
+  
 }
 
