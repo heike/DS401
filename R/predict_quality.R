480e213b693a968a99bf7b04eb07b15a606d7bc3
Heike Hofmann <hofmann@iastate.edu>
2022-04-29 09:06:19 -0500

    update documentation

--- R/predict_quality.R
+++ R/predict_quality.R
@@ -1,58 +1,62 @@
 #' Predicts the probability that a x3p scan will be good and also predicts the reason why it is bad quality if it is predicted to be bad quality.
 #'
-#' @param x3pobjectlist a list of x3p objects to be classified as good or bad quality scans
+#' @param x3p x3p scan of a bullet land
 #' @param x3pnamevector a vector of names of x3p files
-#' @return a dataframe containing a vector of predicted probabilities of a scan being good and its predicted quality_type
+#' @return a dataframe with one row, containing all feature values and a prediction of the scan quality
 #' @import randomForest
 #' @export
 #' @examples
 #' data(fau277_bb_l2)
-#' predict_quality(list(fau277_bb_l2, fau277_bb_l2), x3ptools::x3p_show_xml(fau277_bb_l2,"Comment")$Comment)
-predict_quality<- function(x3pobjectlist, x3pnamevector) {
+#' predict_quality_one(fau277_bb_l2)
+predict_quality_one <- function(x3p, cutoff = 0.57) {
 
-  for(i in 1:((length(x3pobjectlist))))
-  {
-    stopifnot(class(x3pobjectlist[[i]]) == "x3p")
-  }
-
-  data_assess_percentile_na_proportion <- c(rep(0, times = length(x3pobjectlist)))
-  data_assess_bottomempty <- c(rep(0, times = length(x3pobjectlist)))
-  data_assess_col_na <- c(rep(0,times = length(x3pobjectlist)))
-  data_extract_na <- c(rep(0,times = length(x3pobjectlist)))
-  data_assess_middle_na_proportion <- c(rep(0, times = length(x3pobjectlist)))
-  data_assess_rotation <- c(rep(0, times = length(x3pobjectlist)))
-  data_lighting_protocol <- c(rep(0, times = length(x3pobjectlist)))
-
-  newdata <- data.frame(data_assess_percentile_na_proportion, data_assess_col_na, data_extract_na, data_assess_middle_na_proportion, data_assess_rotation, data_lighting_protocol)
-  names(newdata) <- c("assess_percentile_na_proportion", "assess_col_na", "extract_na", "assess_middle_na_proportion", "assess_rotation", "lighting_protocol")
+  stopifnot("x3p" %in% class(x3p))
 
 
-  for(i in 1:dim(newdata)[1]){
-    newdata$assess_percentile_na_proportion[i] <- assess_median_na_proportion(x3pobjectlist[[i]])
-    newdata$assess_bottomempty[i] <- assess_bottomempty(x3pobjectlist[[i]])
-    newdata$assess_col_na[i] <- assess_col_na(x3pobjectlist[[i]])
-    newdata$extract_na[i] <- extract_na(x3pobjectlist[[i]])
-    newdata$assess_middle_na_proportion[i] <- assess_middle_na_proportion(x3pobjectlist[[i]])
-    newdata$assess_rotation[i] <- assess_rotation(x3pobjectlist[[i]])
-    newdata$lighting_protocol[i] <- lighting_protocol(x3pnamevector[i])
-  }
+  data_assess_percentile_na_proportion <- assess_median_na_proportion(x3p)
+  data_assess_bottomempty <- assess_bottomempty(x3p)
+  data_assess_col_na <- assess_col_na(x3p)
+  data_extract_na <- extract_na(x3p)
+  data_assess_middle_na_proportion <- assess_middle_na_proportion(x3p)
+  data_assess_rotation <- assess_rotation(x3p)
+  data_lighting_protocol <- 1 # lighting_protocol(x3p)
+
+  newdata <- data.frame(data_assess_percentile_na_proportion, data_assess_col_na, data_extract_na, data_assess_middle_na_proportion, data_assess_rotation, data_assess_bottomempty, data_lighting_protocol)
+  names(newdata) <- c("assess_percentile_na_proportion", "assess_col_na", "extract_na", "assess_middle_na_proportion", "assess_rotation", "assess_bottomempty", "lighting_protocol")
+  newdata <- newdata %>% mutate(
+    lighting_protocol = factor(lighting_protocol, levels=c(1,2))
+  )
 
   data(randomforest)
   data(randomforest2)
-  Quality <- predict(randomforest, newdata = newdata, type = prob[,2])
-
-  Quality_Type <- c(rep("", times = length(x3pobjectlist)))
-
-  for(i in 1:dim(newdata)){
-    if(Quality[i] <= 0.57){
-      Quality_Type[i] <- predict(randomforest2, newdata = newdata[i], type = 'response')
-    } else{
-      Quality_Type[i] <- "good"
-    }
-
+  reqire(randomForest)
+  newdata$quality <- predict(randomforest, newdata = newdata, type = "prob")[,2]
+  newdata$quality_type <- "good"
+  if (newdata$quality[1] <= cutoff) {
+    newdata$quality_type <- predict(randomforest2, newdata = newdata, type = 'response')
   }
 
-  output <- data.frame(Quality, Quality_Type)
+  return(newdata)
+}
+
+#' Predicts the probability that a x3p scan will be good and also predicts the reason why it is bad quality if it is predicted to be bad quality.
+#'
+#' @param x3plist list of x3p scans of a bullet land
+#' @param x3pnamevector a vector of names of x3p files
+#' @return a dataframe with one row, containing all feature values and a prediction of the scan quality
+#' @import randomForest
+#' @importFrom purrr map
+#' @importFrom tibble tibble
+#' @export
+#' @examples
+#' data(fau277_bb_l2)
+#' predict_quality(list(fau277_bb_l2, fau277_bb_l2), x3pnamevector=c(1,2))
+predict_quality <- function(x3plist, x3pnamevector) {
+browser()
+  output <- tibble(x3p=I(x3plist), x3pname=x3pnamevector)
+  output <- mutate(output,
+    features = purrr::map(x3p, .f = predict_quality_one)
+  )
 
   return(output)
 }
